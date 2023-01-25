b4a09ff90bd79e3d443ff415d7b095b868f9673e
Alen Valdez <alenkvaldeztch@gmail.com>
2023-01-24 23:41:06 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//STEP ONE
+
+string name = "alen";
+int age = 99;
+char middleInitial = 'd';
+bool isOver18 = true;
+double currentTemp = 89.9;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
+//STEP TWO
+
+Console.WriteLine($"{name} was seen with {currentPrice} downtown");
+Console.WriteLine($"i heard that {name} is about {age} years young");
+Console.WriteLine($"{name}'s middle initial is {middleInitial}");
+Console.WriteLine($"i heard that {name} is about {age} years young");
+Console.WriteLine($"with a temparature of {currentTemp}");
+Console.WriteLine($"it is {isOver18} that he is 99 years old");
